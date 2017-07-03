87d9ac2986003aab4aef85851680ef6fbf4d6328
Gianluca Interino <gianluca.interino@ie.ibm.com>
2017-07-03 13:36:25 +0100

    fixed srcollTo functionality and added Lorem Ipsum
    fixed srcollTo functionality and added Lorem Ipsum. Missing scroll to
    for page-top

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
-$('ul.nav').find('a').click(function(){
-    console.log('PIPPPO');
-    var $href = $(this).attr('href');
-    var $anchor = $('#'+$href).offset();
-    console.log($anchor);
-    $('html, body').animate(
-        {
-         scrollTop: $anchor.top
-        },
-        500,
-        'easeOutExpo');
-    return false;
+
+ $(document).ready(function (){
+     console.log("ciccio")
+
+    $('ul.nav').find('a').click(function(){
+        console.log('PIPPPO');
+        var $href = $(this).attr('href');
+        var $anchor = $($href).offset();
+        console.log($anchor);
+        $('html, body').animate(
+            {
+             scrollTop: $anchor.top
+            },
+            5000,
+            'easeOutExpo');
+        return false;
+    });
+
+
 });
