abbd6391c95426d5b4ee087230621fcaecbf359f
Gianluca Interino <gianluca.interino@ie.ibm.com>
2017-07-03 09:04:30 +0100

    new background

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 $('ul.nav').find('a').click(function(){
+    console.log('PIPPPO');
     var $href = $(this).attr('href');
     var $anchor = $('#'+$href).offset();
     console.log($anchor);
-    $('body').animate({ scrollTop: $anchor.top });
+    $('html, body').animate(
+        {
+         scrollTop: $anchor.top
+        },
+        500,
+        'easeOutExpo');
     return false;
 });
